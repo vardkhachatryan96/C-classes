bd6fa270efecabf52e6bc64110f26c5cd3de3faf
vardkhachatryan96 <Varduhi.Khachatryan@helpsystems.com>
2020-08-13 17:22:36 +0400

    Lesson7: Threads

--- Lesson7/Lesson7/ConsoleHost.cs
+++ Lesson7/Lesson7/ConsoleHost.cs
@@ -9,10 +9,6 @@ namespace Lesson7
     {
         internal void Run()
         {
-            var orders1 = this.GetPurchases1(); 
-            var orders2 = this.GetPurchases2();
-            var orders3 = this.GetPurchases3();
-
             object lockObject = new object();
             var orderProducer1 = new PurchasedOrdersProducer(lockObject);
             var orderProducer2 = new PurchasedOrdersProducer(lockObject);
@@ -35,36 +31,6 @@ namespace Lesson7
             orderProcessorThread1.Start();
             orderProcessorThread2.Start();
             orderProcessorThread3.Start();
-        }
-
-        private List<PurchasedOrder> GetPurchases1()
-        {
-            return new List<PurchasedOrder>()
-            {
-                new PurchasedOrder { ItemId = 1, ItemName = "Item1", Count = 2, DeliveryAddress = "Address1" },
-                new PurchasedOrder { ItemId = 2, ItemName = "Item2", Count = 3, DeliveryAddress = "Address2" },
-                new PurchasedOrder { ItemId = 3, ItemName = "Item3", Count = 2, DeliveryAddress = "Address3" },
-            };
-        }
-
-        private List<PurchasedOrder> GetPurchases2()
-        {
-            return new List<PurchasedOrder>()
-            {
-                new PurchasedOrder { ItemId = 4, ItemName = "Item4", Count = 1, DeliveryAddress = "Address3" },
-                new PurchasedOrder { ItemId = 5, ItemName = "Item5", Count = 1, DeliveryAddress = "Address4" },
-                new PurchasedOrder { ItemId = 6, ItemName = "Item6", Count = 5, DeliveryAddress = "Address5" }
-            };
-        }
-
-        private List<PurchasedOrder> GetPurchases3()
-        {
-            return new List<PurchasedOrder>()
-            {
-                new PurchasedOrder { ItemId = 7, ItemName = "Item7", Count = 1, DeliveryAddress = "Address7" },
-                new PurchasedOrder { ItemId = 8, ItemName = "Item8", Count = 1, DeliveryAddress = "Address8" },
-                new PurchasedOrder { ItemId = 9, ItemName = "Item9", Count = 5, DeliveryAddress = "Address9" }
-            };
         }
     }
 }
