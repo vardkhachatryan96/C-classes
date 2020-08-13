c05d825f14642df6da6eb6c9cb8558b48e9f23d6
vardkhachatryan96 <Varduhi.Khachatryan@helpsystems.com>
2020-08-13 19:08:45 +0400

    Lesson7: Threads

---
+++ Lesson7/ConsoleHost.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Lesson7
+{
+    internal class ConsoleHost
+    {
+        internal void Run()
+        {
+        }
+    }
+}
---
+++ Lesson7/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new ConsoleHost();
+
+            host.Run();
+        }
+    }
+}
