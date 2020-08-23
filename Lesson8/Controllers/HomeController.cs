f20436bc8285ebd3de8c60b3408aac47ff6ce13d
vardkhachatryan96 <Varduhi.Khachatryan@helpsystems.com>
2020-08-23 23:18:11 +0400

    Lesson8: Web Api Core

---
+++ Lesson8/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Lesson8.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Lesson8.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IConfiguration config;
+        private readonly CacheController cacheController;
+
+        public HomeController(IConfiguration config, CacheController cacheController)
+        {
+            this.config = config;
+            this.cacheController = cacheController;
+        }
+        private static readonly HttpClient client = new HttpClient();
+
+        [HttpGet("{city}")]
+        public async Task<JsonResult> GetCurWeatherOf(string city)
+        {
+            var key = config.GetValue<string>("ApiKey");
+            if (cacheController.GetCache("CurrWeather") != Ok())
+            {
+
+            }
+            var streamTask = client.GetStreamAsync($"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}");
+            var response = await JsonSerializer.DeserializeAsync<WeatherModel>(await streamTask);
+
+            var result = new JsonResult(new CurrentWeatherModel
+            {
+                CityName = response.name,
+                Humadity = response.main.humidity,
+                Temprature = response.main.temp,
+                Pressure = response.main.pressure
+            });
+
+            cacheController.SetCache("CurrWeather", result);
+
+            return result;
+        }
+    }
+}
---
+++ Lesson8/Startup.cs
@@ -0,0 +1,56 @@
+using EasyCaching.Core.Configurations;
+using Lesson8.Controllers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Lesson8
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEasyCaching(options =>
+            {
+                options.UseRedis(redisConfig =>
+                {
+                    redisConfig.DBConfig.Endpoints.Add(new ServerEndPoint("localhost", 44322));
+                    redisConfig.DBConfig.AllowAdmin = true;
+                },
+                "redis");
+            });
+            services.AddTransient<CacheController, CacheController>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
